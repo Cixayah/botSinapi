c26bd0f1fa6bcd1abb80c5d6cd868dab0f63d82f
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-06 10:58:16 -0300

    #

--- Chrome-bin/sinapi.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:62639f2a35322eece49d8294fcd2c726bd0d633d6be3310510ece3f0bcdf4a3e
-size 2458
---
+++ botSinapi/sinapi.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
+
+# Variável de data
+mesAno = "202404"
+
+# Obter o diretório atual do script
+diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+
+# Caminho para o ChromeDriver no diretório atual
+chrome_driver_path = os.path.join(diretorio_atual, "chromedriver.exe")
+
+# Configurar as opções do Chrome
+chrome_options = webdriver.ChromeOptions()
+# Adicione as opções adicionais do Chrome, se necessário
+# chrome_options.add_argument("--headless")  # Exemplo: Executar em modo headless
+
+# Inicializar o serviço do ChromeDriver
+driver_service = webdriver.chrome.service.Service(chrome_driver_path)
+
+# Inicializar o driver com o serviço e as opções configuradas
+driver = webdriver.Chrome(service=driver_service, options=chrome_options)
+
+# URL da página que você deseja visitar
+url = "https://www.caixa.gov.br/site/Paginas/downloads.aspx"
+
+# Estados requeridos
+estados = [
+    "AL",
+    "BA",
+    "CE",
+    "DF",
+    "MA",
+    "MG",
+    "MS",
+    "PA",
+    "PB",
+    "PE",
+    "PR",
+    "RJ",
+    "RO",
+    "SE",
+    "SP",
+]
+
+# Iniciar a sessão do navegador e abrir a página
+driver.get(url)
+
+# Encontre o campo de busca
+busca_textbox = driver.find_element(By.ID, "txtBusca")
+
+# Iterar pelos estados e fazer a busca por cada estado
+for estado in estados:
+    termo_de_busca = f"{estado}_{mesAno}_NaoDesonerado"
+    busca_textbox.clear()
+    busca_textbox.send_keys(termo_de_busca)
+
+    # Localize e clique no botão de busca usando o XPath fornecido
+    search_button = driver.find_element(
+        By.XPATH,
+        "/html/body/form/div[4]/div/div/div[3]/div/span/div[1]/div[2]/div/div/div[1]/div[1]/div[1]/div/button/i",
+    )
+    search_button.click()
+    time.sleep(15)
+    # Aguardar até que a página de resultados esteja completamente carregada (você pode ajustar o tempo limite)
+    wait = WebDriverWait(driver, 10)
+    wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, f"//a[contains(@href, '{termo_de_busca}')]")
+        )
+    )
+
+    # Encontrar todos os links que contêm o termo de busca no atributo href
+    links = driver.find_elements(By.XPATH, f"//a[contains(@href, '{termo_de_busca}')]")
+
+    # Iterar sobre os links e efetuar um clique com um intervalo de 1 segundo entre cada um
+    for link in links:
+        link.click()
+        time.sleep(10)
+
+# Fechar o navegador após a automação
+time.sleep(15)
+driver.quit()
